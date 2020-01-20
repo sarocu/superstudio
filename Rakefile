19639cadec81d23870b97c8316fa201817bcbfc3
Sam <sam.currie@simuwatt.com>
2020-01-20 13:56:21 -0700

    adding CLI tests to Rakefile tasks

---
+++ Rakefile
@@ -0,0 +1,10 @@
+require "bundler/gem_tasks"
+task :default => :spec
+
+task :test_update do
+    sh "./test/test_update_model.sh"
+end
+
+task :test_create do
+    sh "./test/test_create_standard_model.sh"
+end
--- test/test_update_model.sh
+++ test/test_update_model.sh
@@ -1,1 +1,1 @@
-../bin/superstudio --debug --update --model ./denver-model.osm --geometry --zoning per-space --wwr 0.3 --floor_area 2500 --systems --template centralSystemHwChwVav
+./bin/superstudio --debug --update --model ./test/denver-model.osm --geometry --zoning per-space --wwr 0.3 --floor_area 2500 --systems --template bootstrapCentralHwChwVavBasic
